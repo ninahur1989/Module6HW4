7a880070a43edffebadb172b3696fa1613c4cb5a
stas <79771234+ninahur1989@users.noreply.github.com>
2022-09-05 19:21:54 +0300

    starter app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M6HW3RazorPages.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
